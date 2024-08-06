cb330478a316213e2f9d62a1a37ec7b86e0759eb
Anshuman <chaoanxuman999@gmail.com>
2024-08-07 01:18:28 +0530

    DTO class created for interacting with database

--- demo/src/main/java/com/movieflix/DTO/MovieDTO.java
+++ demo/src/main/java/com/movieflix/DTO/MovieDTO.java
@@ -1,50 +1,49 @@
-package com.movieflix.Model;
-
+package com.movieflix.DTO;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.Set;
 
+
+@Data //remove boiler plate getter and setter as we will use this class to get repsonse and set response
 @NoArgsConstructor
 @AllArgsConstructor
-@Getter
+public class MovieDTO { //DTO class do not need  DB level Validation
 
-@Entity
-@Table(name = "moviepi")
-public class Movie {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer movieId;
 
-    @Column(nullable = false,length = 200)
+
     @NotBlank(message = "Please provide movie title")
     private String title;
-    @Column(nullable = false)
+
     @NotBlank(message = "Please provide movie director")
     private String director;
-    @Column(nullable = false)
+
     @NotBlank(message = "Please provide movie studio name")
     private String studio;
 
-    @ElementCollection
+
     @CollectionTable(name = "movie_cast") //create a table inside and also can chaneg with the change in entity
     private Set<String> movieCast;
 
 
 
-    @Column(nullable = false)
+
     @NotBlank(message = "Please provide the poster")
     private String poster;
 
 
 
-    @Column(nullable = false)
     private Integer releaseYear;
 
 
+    @NotBlank(message = "Please Provide poster url")
+    private String posterUrl;
 }
--- demo/src/main/java/com/movieflix/Repository/MovieRepository.java
+++ demo/src/main/java/com/movieflix/Repository/MovieRepository.java
@@ -4,4 +4,5 @@ import com.movieflix.Model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MovieRepository extends JpaRepository<Movie,Integer> {
+
 }
